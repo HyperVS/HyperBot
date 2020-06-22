97b29d2b8c364caefbe9e70391816dc8f531f171
HypervTV <hyperxrv@gmail.com>
2020-06-22 04:44:47 -0400

    Updated schedule command.

--- commands/schedule.js
+++ commands/schedule.js
@@ -11,7 +11,7 @@ module.exports = {
         const embed = new MessageEmbed();
 		embed.setTitle('Here\'s a link to the team schedule:');
 		embed.setColor(colorOne);
-        embed.addField('https://docs.google.com/document/d/1dw_rNoB4zeA0oA7ogxclhxlbbz99kcSmnbbG9m4Bo0w/edit?usp=sharing');
+        embed.setDescription('https://docs.google.com/document/d/1dw_rNoB4zeA0oA7ogxclhxlbbz99kcSmnbbG9m4Bo0w/edit?usp=sharing');
         return message.channel.send(embed);
     }
 }
