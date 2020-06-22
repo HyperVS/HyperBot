4de1dd73a6289d579a3f1ad8b8fb31967b59b260
HypervTV <hyperxrv@gmail.com>
2020-06-22 04:40:04 -0400

    Updated schedule command.

--- commands/schedule.js
+++ commands/schedule.js
@@ -1,3 +1,6 @@
+const { colorOne } = require('../config.json');
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'schedule',
 	aliases: [],
@@ -5,6 +8,10 @@ module.exports = {
 	args: false,
     private: false,
 	execute(message, args){ 
-        return message.channel.send('https://docs.google.com/document/d/1dw_rNoB4zeA0oA7ogxclhxlbbz99kcSmnbbG9m4Bo0w/edit?usp=sharing');
+        const embed = new MessageEmbed();
+		embed.setTitle('Here\'s a link to the team schedule:');
+		embed.setColor(colorOne);
+        embed.addField('https://docs.google.com/document/d/1dw_rNoB4zeA0oA7ogxclhxlbbz99kcSmnbbG9m4Bo0w/edit?usp=sharing');
+        return message.channel.send(embed);
     }
 }
