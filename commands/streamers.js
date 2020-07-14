dad20929657872c96fd34fc418b92e320136c208
HypervTV <hyperxrv@gmail.com>
2020-07-14 18:56:39 -0400

    Added streamer

--- commands/streamers.js
+++ commands/streamers.js
@@ -1,16 +1,17 @@
 const { bloopy, cowboys, fourzone, gwacieee, rightize, shwn, suave, twisted, lawless, twitch, colorOne, logan } = require('../config.json');
 const { MessageEmbed } = require('discord.js');
 const streamers = [
-    {name: "bloopy", link: "https://www.twitch.tv/blooploop"},
-	{name:"fourzone", link: "https://www.twitch.tv/fourzone3"},
-	{name:"gwacieee", link: "https://www.twitch.tv/gwacieee"},
-	{name:"rightize", link: "https://www.twitch.tv/rightize"},
-	{name:"shwn", link: "https://www.twitch.tv/shwn_tv"},
-	{name:"suave", link: "https://www.twitch.tv/suavehabibi"},
-	{name:"twisted", link: "https://www.twitch.tv/twistedtweak01"},
-	{name:"lawless", link: "https://www.twitch.tv/lawlessbeats"},
-	{name:"logan", link: "https://www.twitch.tv/logionekenobi"},
-	{name:"cowboys", link: "https://www.twitch.tv/cowboys_0988"}
+    {name: "Bloopy", link: "https://www.twitch.tv/blooploop"},
+	{name:"FourZone", link: "https://www.twitch.tv/fourzone3"},
+	{name:"Gwacieee", link: "https://www.twitch.tv/gwacieee"},
+	{name: "Maiku", link: "https://www.twitch.tv/maikulol"},
+	{name:"Rightize", link: "https://www.twitch.tv/rightize"},
+	{name:"Shwn", link: "https://www.twitch.tv/shwn_tv"},
+	{name:"Suave", link: "https://www.twitch.tv/suavehabibi"},
+	{name:"Twisted", link: "https://www.twitch.tv/twistedtweak01"},
+	{name:"Lawless", link: "https://www.twitch.tv/lawlessbeats"},
+	{name:"Logan", link: "https://www.twitch.tv/logionekenobi"},
+	{name:"Cowboys", link: "https://www.twitch.tv/cowboys_0988"}
 ];
 
 module.exports = {
