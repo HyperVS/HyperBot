a1b0d4ddc95cceae422dd9e83ad4dcb319168d25
HypervTV <hyperxrv@gmail.com>
2020-07-14 18:11:03 -0400

    Optmized streamers.js

--- commands/streamers.js
+++ commands/streamers.js
@@ -1,5 +1,17 @@
 const { bloopy, cowboys, fourzone, gwacieee, rightize, shwn, suave, twisted, lawless, twitch, colorOne, logan } = require('../config.json');
 const { MessageEmbed } = require('discord.js');
+const streamers = [
+    {name: "bloopy", link: "https://www.twitch.tv/blooploop"},
+	{name:"fourzone", link: "https://www.twitch.tv/fourzone3"},
+	{name:"gwacieee", link: "https://www.twitch.tv/gwacieee"},
+	{name:"rightize", link: "https://www.twitch.tv/rightize"},
+	{name:"shwn", link: "https://www.twitch.tv/shwn_tv"},
+	{name:"suave", link: "https://www.twitch.tv/suavehabibi"},
+	{name:"twisted", link: "https://www.twitch.tv/twistedtweak01"},
+	{name:"lawless", link: "https://www.twitch.tv/lawlessbeats"},
+	{name:"logan", link: "https://www.twitch.tv/logionekenobi"},
+	{name:"cowboys", link: "https://www.twitch.tv/cowboys_0988"}
+];
 
 module.exports = {
 	name: 'streamers',
@@ -12,17 +24,10 @@ module.exports = {
         const embed = new MessageEmbed();
 		embed.setTitle('Here\'s a list of all streamers in the discord:');
         embed.setColor(colorOne);
-        embed.addField("BloopLoop", bloopy);
-        embed.addField("Cowboys_0988", cowboys);
-        embed.addField("FourZone", fourzone);
-        embed.addField("Gwacieee", gwacieee);
-        embed.addField("HypervTV", twitch);
-        embed.addField("Lawlessbeats", lawless);
-        embed.addField("LogiOneKenobi", logan);
-        embed.addField("Rightize", rightize);
-        embed.addField("Shwn", shwn);
-        embed.addField("SuaveHabibi", suave);
-        embed.addField("Twisted", twisted);
+        streamers.forEach((streamer) => {
+            embed.addField(streamer.name, streamer.link)
+        });
+
         return message.channel.send(embed);
     }
 }
